6d56ecdaf59aa3348a319266beab0d33b741056f
Rashaad <rashaad.meyer@gmail.com>
2023-03-02 14:33:19 +0200

    TensorFlow Tutorial 6 improved

--- TensorFlow/06_custom_layer_w_custom_grad.py
+++ TensorFlow/06_custom_layer_w_custom_grad.py
@@ -2,20 +2,23 @@ import tensorflow as tf
 
 
 class MyDenseLayer(tf.keras.layers.Layer):
-    def __init__(self, input_size, hidden_size):
+    def __init__(self, hidden_size):
         super(MyDenseLayer, self).__init__()
         self.w = None
         self.b = None
         self.hidden_size = hidden_size
 
+    def build(self, input_shape):
+
         self.w = self.add_weight(
             name='w',
-            shape=(input_size, self.hidden_size),
+            shape=(input_shape[-1], self.hidden_size),
             initializer='random_normal',
             trainable=True
         )
+
         self.b = self.add_weight(
-            name='b',
+            name='w',
             shape=(self.hidden_size,),
             initializer='random_normal',
             trainable=True
@@ -45,44 +48,41 @@ def custom_operation(w, b, x):
 
 if __name__ == '__main__':
     (x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
-    x_train = x_train.reshape(-1, 28 * 28).astype("float32") / 255.0
-    x_test = x_test.reshape(-1, 28 * 28).astype("float32") / 255.0
+    x_train = x_train.reshape(-1, 28 * 28).astype('float32') / 255.0
+    x_test = x_test.reshape(-1, 28 * 28).astype('float32') / 255.0
 
     print('Custom Dense Layer')
     print('-' * 30)
 
     model_0 = tf.keras.Sequential([
         tf.keras.layers.Input(shape=(28 * 28)),
-        MyDenseLayer(28 * 28, 64),
-        MyDenseLayer(64, 32),
-        MyDenseLayer(32, 10)
+        MyDenseLayer(64),
+        MyDenseLayer(32),
+        MyDenseLayer(10),
     ])
 
-    model_0.compile(
-        loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
-        optimizer=tf.keras.optimizers.Adam(),
-        metrics=["accuracy"],
-    )
+    model_0.compile(loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+                    optimizer=tf.keras.optimizers.Adam(),
+                    metrics=['accuracy'])
 
     model_0.fit(x_train, y_train, epochs=5)
     model_0.evaluate(x_test, y_test)
     print('=' * 30)
+
     print('TensorFlow Dense Layer')
     print('-' * 30)
 
-    model_1 = tf.keras.Sequential([
+    model_0 = tf.keras.Sequential([
         tf.keras.layers.Input(shape=(28 * 28)),
-        tf.keras.layers.Dense(28 * 28),
         tf.keras.layers.Dense(64),
-        tf.keras.layers.Dense(32)
+        tf.keras.layers.Dense(32),
+        tf.keras.layers.Dense(10),
     ])
 
-    model_1.compile(
-        loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
-        optimizer=tf.keras.optimizers.Adam(),
-        metrics=["accuracy"],
-    )
+    model_0.compile(loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+                    optimizer=tf.keras.optimizers.Adam(),
+                    metrics=['accuracy'])
 
-    model_1.fit(x_train, y_train, epochs=5)
-    model_1.evaluate(x_test, y_test)
+    model_0.fit(x_train, y_train, epochs=5)
+    model_0.evaluate(x_test, y_test)
     print('=' * 30)
